198da34111d8b2576657b8baabddd159f392331b
noiseArch <mativalto@gmail.com>
2026-06-13 14:17:17 -0300

    scroll to top al navegar

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,6 +1,7 @@
 import "./helpers/editorConfig";
 import './App.css';
 import { HashRouter, Routes, Route } from "react-router-dom"
+import ScrollToTop from './ScrollToTop';
 import './i18n';
 import { BookView } from "./components/Book/BookView";
 import { pdep } from "./components/Book/Book.data";
@@ -11,19 +12,14 @@ import Exercise from "./Exercise/Exercise";
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<BookView book={pdep}/>} />
         <Route path="/chapters/:chapterId" element={<Chapter />} />
-        <Route path="/lessons/:lessonId" element={<Lesson/>} />
-        <Route path="/lessons/:lessonId/exercises/:exerciseId" element={<Exercise />} />
+        <Route path="/chapters/:chapterId/lessons/:lessonId" element={<Lesson/>} />
+        <Route path="/chapters/:chapterId/lessons/:lessonId/exercises/:exerciseId" element={<Exercise />} />
       </Routes>
     </HashRouter>
   );
 }
 export default App;
-
-/**
- * 
-        {/* <Route path="/chapters/:chapterId/appendix" element={<Appendix />} /> }
-        {/* <Route path="/faqs" element={<Faqs />} /> }
- */
